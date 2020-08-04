7bc97c28ae224f02ad80b77a724de8ac962cb8eb
nick <neserag95@gmail.com>
2020-08-04 18:22:25 +0300

    Error templates, security, etc.

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +12,6 @@ class ForumController extends AbstractController
      */
     public function home()
     {
-        /** @var User $user */
-        $user = $this->getUser();
-
-        return $this->render('forum/homepage.html.twig', [
-            'user' => $user->getFirstName()
-        ]);
+        return $this->render('forum/homepage.html.twig');
     }
 }
