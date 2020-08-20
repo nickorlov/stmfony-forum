797f9338611d5b6259811ebada4480c7b817dcd7
nick <neserag95@gmail.com>
2020-08-20 18:08:35 +0300

    Set up profile form, avatar uploading

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,21 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\ProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     /**
      * @Route("/profile")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function profile()
+    public function profile(Request $request)
     {
-        $form = $this->createForm(ProfileType::class);
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $avatar */
+            $avatar = $form->get('avatar')->getData();
+            if ($avatar) {
+                $originalFilename = pathinfo($avatar->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $avatar->guessExtension();
+
+                try {
+                    $avatar->move(
+                        $this->getParameter('avatars_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $user->setAvatar($newFilename);
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_profile_profile');
+        }
 
         return $this->render('profile/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    private function generateUniqueFileName(): string
+    {
+        return md5(uniqid());
     }
 }
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProfileType extends AbstractType
 {
@@ -15,7 +17,20 @@ class ProfileType extends AbstractType
         $builder
             ->add('firstName')
             ->add('lastName')
-            ->add('avatar', FileType::class)
+            ->add('avatar', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image',
+                    ])
+                ],
+            ])
             ->add('age')
             ->add('gender')
             ->add('city');
